5b31e911f9769ac12b77b31cf02ac5f16a35891f
xP-xD <tlsqlfn842@naver.com>
2023-08-06 23:15:40 +0900

    8th commit

---
+++ src/baekjoonClass2/P1018.java
@@ -0,0 +1,26 @@
+package baekjoonClass2;
+
+import java.util.Scanner;
+
+public class P1018 {
+
+	public static void main(String[] args) {
+
+		//https://www.acmicpc.net/problem/1018
+		
+		Scanner scanner = new Scanner(System.in);
+
+		//보드
+		int N = scanner.nextInt();
+		int M = scanner.nextInt();
+		
+		//컬러
+		//인접한 컬러는 무조건 다른 컬러여야함 +-1은 다른 컬러
+		char B = 'B';
+		char W = 'W';
+		
+		
+		scanner.close();
+	}
+
+}
---
+++ src/baekjoonClass2/P1546.java
@@ -0,0 +1,48 @@
+package baekjoonClass2;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class P1546 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//https://www.acmicpc.net/problem/1546
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		ArrayList<Integer> numberList = new ArrayList<Integer>();
+		
+		int M = 0;
+		int N = scanner.nextInt();
+		float sum = 0;
+		float avg = 0;
+		
+		//값들을 배열에 저장
+		for(int i =0; i <N; i++) {
+			numberList.add(scanner.nextInt());
+		}
+
+		//역정렬
+		Collections.sort(numberList, Collections.reverseOrder());
+		System.out.println(numberList);
+		
+		for(Integer number : numberList) {
+			//최고점
+			M = numberList.get(0);
+			//공식 대입
+			float fixedScore = (float)number/M * 100;
+			sum += fixedScore;
+			
+		}
+		
+		avg = sum/numberList.size();
+		
+		System.out.println(avg);
+		
+		scanner.close();
+	}
+
+}
